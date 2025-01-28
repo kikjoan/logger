6fcd89c8a052fbc0cca33acc41e0dfbf1b8b0dd0
alex <kikjoan@vk.com>
2025-01-28 14:49:02 +0300

    logger lib

---
+++ src/app/logger_app.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Александр Юферов on 28.01.2025.
+//
+
+#include "logger_app.h"
---
+++ src/app/logger_app.h
@@ -0,0 +1,16 @@
+//
+// Created by Александр Юферов on 28.01.2025.
+//
+
+#ifndef LOGGER_APP_H
+#define LOGGER_APP_H
+
+
+
+class logger_app {
+
+};
+
+
+
+#endif //LOGGER_APP_H
